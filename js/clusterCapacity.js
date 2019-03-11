2b79b733ded93d303adf2236654860913b5b4730
Bruno Furtado <bffurtado@gmail.com>
2019-03-10 21:34:03 -0300

    Initial commit

---
+++ js/clusterCapacity.js
@@ -0,0 +1,57 @@
+/*
+
+*/
+
+
+function clearForm() {
+  document.getElementById('clusterCapacity').reset();
+}
+
+function calculateBasedMem() {
+	//var theForm = document.forms["capacity_form"];
+  var qtyAppNode = document.getElementById('qtyAppNode').value;
+  var appNodeMem = document.getElementById('memNodeSize').value;
+  var memPodSize = document.getElementById('memPodSize').value;
+  var totalAppNodeMem = appNodeMem * qtyAppNode;
+  var totalPodPerMem = totalAppNodeMem / memPodSize;
+  return totalPodPerMem;
+	//display the result
+  //document.getElementById('ccapacity').innerHTML = "Total pod por Mem: "+totalPodPerMem;
+}
+
+function calculateBasedCpu() {
+  var qtyAppNode = document.getElementById('qtyAppNode').value;
+  var appNodeCpu = document.getElementById('cpuNodeSize').value;
+  var cpuPodSize = document.getElementById('cpuPodSize').value;
+  var totalAppNodeCpu = appNodeCpu * qtyAppNode;
+  var totalPodPerCpu = totalAppNodeCpu / cpuPodSize;
+  return totalPodPerCpu;
+	//display the result
+  //document.getElementById('ccapacity').innerHTML = "Total pod por CPU: "+totalPodPerCpu;
+}
+
+function compare() {
+
+	var basedMem = Math.floor(calculateBasedMem());
+	var basedCpu = Math.floor(calculateBasedCpu());
+	if (basedMem > basedCpu){
+	  //display the result
+    var divobj = document.getElementById('cluster_capacity');
+    divobj.style.display='block';
+    divobj.innerHTML = "Total pod por Mem: "+basedMem;
+	} else {
+	  //display the result
+    var divobj = document.getElementById('cluster_capacity');
+    divobj.style.display='block';
+    divobj.innerHTML = "Total pod por CPU: "+basedCpu;
+	}
+	
+}
+
+function hideTotal()
+{
+    var divobj = document.getElementById('cluster_capacity');
+    divobj.style.display='none';
+}
+
+
